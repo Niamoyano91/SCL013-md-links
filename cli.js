2d5c4f38fec4a200a08c7b15edd59268a626bc9c
estefania moyano <estefaniamoyano@Airdeestefania.home>
2020-08-05 17:38:44 -0400

    funcion mdLinks

--- cli.js
+++ cli.js
@@ -1,55 +1,60 @@
-const searchMd = require('./index')
+const mdLinks = require('./index');
+let validate = require('./index');
+let stats = require('./index');
 const path = require("path");
 const colors = require('colors');
 
-let stats = false;
-let validate = false;
-let help = false;
 
 
 //funcion que valida lo que ingresa usuario en la terminal//
-
-
-
-
 const flags = () => {
-    if (process.argv.includes('--validate') || process.argv.includes('--v')) {
-        validate = true;
-        stats = false;
-        help = false;
-        searchMd
-        console.log(validate,stats,help)
-        console.log('ENTRO EL FLAGS --V')
-    } if (process.argv.includes('--stats') || process.argv.includes('--s')) {
-        validate = false;
-        stats = true;
-        help = false;
-        console.log('ENTRO EL FLAGS --S')
-    } if ((process.argv.includes('--validate') && process.argv.includes('--status')) || ((process.argv.includes('--v') && process.argv.includes('--s')))) {
-        validate = true;
-        stats = true;
-        help = false;
-        console.log('ENTRO EL FLAGS --S & --V')
-    } if (process.argv.includes('--h') || process.argv.includes('--help')) {
-        help = true;
-        validate = false;
-        stats = false;
-    }    if ((process.argv[2] === './ ')|| (process.argv[2] === undefined)) {
-        let route = __dirname;
-        process.argv[2]= './'
-        searchMd
-        console.log('ENTRO2');//CHARLIE//
-        return
-      } if (path.extname(process.argv[2]) === '.md') {
-        let route = process.argv[2];
-        searchMd;
-        console.log('es un archivo extension .md');//CHARLIE//
-        return
-      } else {
-        console.log('Ingresa ruta de un directorio o archivo valido'.rainbow);
-        return
-      }
+  if (process.argv.includes('--h') || process.argv.includes('--help')) {
+    help = true;
+    validate = false;
+    stats = false;
+    return
+  } if ((process.argv.includes('--validate') && process.argv.includes('--status')) ||
+    ((process.argv.includes('--v') && process.argv.includes('--s')))) {
+    validate = true;
+    stats = true;
+    help = false;
+    mdLinks.mdLinks(validate, stats).then(A=> console.log(A))
+    console.log('ENTRO EL FLAGS --S & --V')
+    return
+  } if (process.argv.includes('--stats') || process.argv.includes('--s')) {
+    stats = true;
+    validate = false;
+    help = false;
+    mdLinks.mdLinks(validate, stats).then(A=> console.log(A))
+    console.log(validate, stats, help)
+    console.log('ENTRO EL FLAGS --S')
+    return
+  } else if (process.argv.includes('--validate') || process.argv.includes('--v')) {
+    validate = true;
+    stats = false;
+    help = false;
+    console.log(validate)
+    mdLinks.mdLinks(validate, stats).then(A=> console.log(A))
+    console.log(validate, stats, help)
+    console.log('ENTRO EL FLAGS --V')
+    return
+  } else if ((process.argv[2] === './') || (process.argv[2] === undefined)) {
+    validate = false;
+    stats= false;
+    let route = __dirname;
+    process.argv[2] = './'
+    mdLinks.mdLinks(validate, stats).then(A=> console.log(A))
+    console.log('ENTRO2');//CHARLIE//
+    return
+  } else if (path.extname(process.argv[2]) === '.md') {
+    validate= true
+    let route = process.argv[2];
+    mdLinks.mdLinks(validate).then(A=> console.log(A))
+    console.log('es un archivo extension .md');//CHARLIE//
+    return
+  } else {
+    console.log('Ingresa ruta de un directorio o archivo valido'.rainbow);
+    return
+  }
 }
 flags()
-
-
--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 const fs = require('fs');
 const referencesLinks = /\[([^\]]*)\]\(([^)]*)\)/g;
-
 const path = require("path");
 const fetch = require("node-fetch");
-
+let stats = false;
+let validate = false;
+let help = false;
 
 
 //Funcion Integradora que busca el archivo extension .md//
-const searchMd = () => {
-  enterFolder(path.resolve()).then(archivos => {
-    archivos.forEach((archivo) => {
-      if (path.extname(archivo) === '.md') {
-        readSaveLinks(archivo)
-      }
-    })
-  })
+/*const searchMd = () => {
+enterFolder(path.resolve()).then(files => {
+files.forEach((file) => {
+if (path.extname(file) === '.md') {
+(readSaveLinks(file))
 }
+})
+})
+};*/
 
 
 //funcion que entra en la carpeta//
@@ -29,57 +30,145 @@ const enterFolder = (route) => {
       }
     })
   });
-}
+};
 
 
 //Funcion que extrae links//
 
-const readSaveLinks = (e) => {
-  const linksArchive = []
+/*const readSaveLinks = (e) => {
+const linksArchive = []
+return new Promise((resolve, reject) => {
+fs.readFile(e, 'utf-8', (err, data) => {
+if (err) {
+reject(err)
+} else {
+const allLinks = data.match(referencesLinks);
+allLinks.forEach(obj => {
+linksArchive.push({
+href: obj.split('](')[1].slice(0, -1),
+text: obj.split('](')[0].slice(1),
+file: e
+})
+})
+validateLinks(linksArchive)
+resolve(linksArchive)
+}
+})
+})
+};*/
+
+const mdLinks = (validate,stats) => {
+  //return Promise.resolve('AQUI')
   return new Promise((resolve, reject) => {
-    fs.readFile(e, 'utf-8', (err, data) => {
-      if (err) {
-        reject(err)
-      } else {
-        const allLinks = data.match(referencesLinks);
-        allLinks.forEach(obj => {
-          linksArchive.push({
-            href: obj.split('](')[1].slice(0, -1),
-            text: obj.split('](')[0].slice(1),
-            file: e
+  enterFolder(path.resolve()).then(files => {
+    files.forEach((file) => {
+      if (path.extname(file) === '.md') {
+        
+          fs.readFile(file, 'utf-8', (err, data) => {
+            if (err) {
+              return reject(err);
+            }
+            const linksArchive = []
+            const allLinks = data.match(referencesLinks);
+            allLinks.forEach(obj => {
+              linksArchive.push({
+                href: obj.split('](')[1].slice(0, -1),
+                text: obj.split('](')[0].slice(1),
+                file: file
+              })
+            })
+           if (validate === true && stats === true) {
+            statusLinksBroken(linksArchive)
+            statusLinks(linksArchive)
+            console.log('STATS')
+            return
+           }else if (validate === false && stats === false) {
+              console.log(validate)
+              resolve(linksArchive);
+              return 
+            } else if (validate === true && stats === false) {
+              console.log('else if', validate)
+              validateLinks(linksArchive).then(a=>console.log(a))
+              return
+            } else if (stats === true && validate === false) {
+              statusLinks(linksArchive)
+              console.log('STATS')
+              return
+            } 
+
           })
-        })
-        validateLinks(linksArchive)
-        resolve(linksArchive)
       }
     })
   })
+})
 }
- //valida el estado de los links//
+
+
+//valida el estado de los links//
 const validateLinks = (links) => {
-  links.map(actualLink => {
-    //console.log(actualLink.href)
-    fetch(actualLink.href)
-      .then(response => {
-        if (response.status === 200) {
-          console.log('Link: ' + actualLink.href + ' ' + 'Estado: ' + response.statusText + ' ' + response.status)//CHARLIE//
-        } else {
-          console.log('Link: ' + actualLink.href + ' ' + 'Estado: ' + response.statusText + ' ' + response.status)//CHARLIE//
-
-        }
-
-
-      })
+  return new Promise((resolve, reject) => {
+    links.map(actualLink => {
+      //console.log(actualLink.href)
+      fetch(actualLink.href)
+        .then(response => {
+          console.log(response.url)
+          if (response.status) {
+            resolve('Link: ' + actualLink.href + ' ' + 'Estado: ' + response.statusText + ' ' + response.status + '  ' + actualLink.text)//CHARLIE//
+          } else {
+            reject(err)
+          }
+        })
+    })
   })
 }
+
+
+const statusLinks = (links) => {
+  let numOfLinks = [];
+  links.forEach(element => {
+    numOfLinks.push(element.href);
+  });
+  let uniqueLinks = new Set(numOfLinks);
+  console.log(
+    ' Total: ' + numOfLinks.length,
+    '\n', 'Unique: ' + uniqueLinks.size
+  );
+}
+
+const statusLinksBroken = (links) => {
+  return new Promise ((resolve, reject)=> {
+    let allLinks = [];
+    let countBroken = 0;
+    links.forEach((link)=> {allLinks.push(fetch(link.href)
+      .then(response => {
+        return {
+        ...link,
+        status: response.status
+        }
+      })
+      .catch((err)=> {
+        return {
+          ...link,
+          status: `Fail 404, ${err.message}`
+        }
+      }))
+    })
+    resolve(Promise.all(allLinks))
+  })
+  .then(resolve => {
+    const countBroken= resolve.filter(link =>link.status!= 200)
+    const linksBroken= [];
+    linksBroken.push(countBroken.length)
+    console.log(' Broken: ',countBroken.length)
+  })
+  };
 
 
 
 
 module.exports = {
-  searchMd: searchMd(),
+  mdLinks: mdLinks,
+  stats: stats,
+  validate: validate,
+  validateLinks: validateLinks
 }
-
-
-
-
